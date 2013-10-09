e7dcd94d2acb9085c10344900daa74b867134d88
Bo Sun <sunbocsg@gmail.com>
2013-10-09 12:54:29 +0800

    Second Activity Display Message

--- cs3281simple/src/com/example/cs3281simple/MainActivity.java
+++ cs3281simple/src/com/example/cs3281simple/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.cs3281simple;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,6 +26,11 @@ public class MainActivity extends Activity {
 	public void sendMessage(View view) {
 	    // Do something in response to button
 		System.out.println("Hello cs3281");
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
 
 }
