e6cb6bd3ae6eb985f7015a110f64e6cb90771ab0
Bo Sun <sunbocsg@gmail.com>
2013-10-15 15:10:32 +0800

    Change Package Name

---
+++ cs3281code/src/com/example/cs3281/GetDataFromSunfire.java
@@ -0,0 +1,101 @@
+package com.example.cs3281;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+ 
+import javax.net.ssl.HttpsURLConnection;
+ 
+public class GetDataFromSunfire{
+ 
+	private final String USER_AGENT = "Mozilla/5.0";
+ /*
+	public static void main(String[] args) throws Exception {
+ 
+		GetDataFromSunfire http = new GetDataFromSunfire();
+ 
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet();
+ 
+		System.out.println("\nTesting 2 - Send Http POST request");
+		http.sendPost();
+ 
+	}
+ */
+	// HTTP GET request
+	public String sendGet() throws Exception {
+ 
+		String url = "http://www.comp.nus.edu.sg/~zhaoyue/3281.json";
+ 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		// optional default is GET
+		con.setRequestMethod("GET");
+ 
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+ 
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		//print result
+		return response.toString();
+ 
+	}
+ 
+	// HTTP POST request
+	public void sendPost() throws Exception {
+ 
+		String url = "https://selfsolve.apple.com/wcResults.do";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+ 
+		//add request header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+ 
+		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+ 
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+ 
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		//print result
+		System.out.println(response.toString());
+ 
+	}
+ 
+}
